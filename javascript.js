9c9a84219e2998a28aacf95dbc491b2a8f400e4b
Jara86 <jarouschka@gmail.com>
2024-11-17 17:00:58 +0100

    addeJavascript function anc corected HTML

--- javascript.js
+++ javascript.js
@@ -12,6 +12,7 @@ function updateCityTime(cityId, timezone) {
 
 function updateTime() {
     const cities = [
+        { id: "current-position", timezone: "Europe/London" },
         { id: "Stockholm", timezone: "Europe/Stockholm" },
         { id: "Canberra", timezone: "Australia/Canberra" },
         { id: "Toronto", timezone: "America/Toronto" }
@@ -26,18 +27,21 @@ function updateCity(event) {
         cityTimeZone = moment.tz.guess();
     }
 
-    const cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    const cityName = cityTimeZone.split("/")[1].replace("_", " ");
     const cityTime = moment().tz(cityTimeZone);
     const citiesElement = document.querySelector("#cities");
     
     citiesElement.innerHTML = `
-    <div class="city">
-        <div>
-            <h2>${cityName}</h2>
-            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+        <div class="city">
+            <div>
+                <h2>${cityName}</h2>
+                <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+            </div>
+            <div class="time">
+                ${cityTime.format("h:mm:ss")} 
+                <small>${cityTime.format("A")}</small>
+            </div>
         </div>
-        <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
-    </div>
     `;
 }
 
@@ -46,5 +50,5 @@ updateTime();
 setInterval(updateTime, 1000);
 
 // Set up city selector
-const citiesSelectElement = document.querySelector("#city");
-citiesSelectElement.addEventListener("change", updateCity);
+const citySelect = document.querySelector("#city-select");
+citySelect.addEventListener("change", updateCity);
